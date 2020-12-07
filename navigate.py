1d45ad91baa165fbb01f282c833dcad5235fc1ed
Aphrx <amalnnath20@yahoo.ca>
2020-12-06 19:40:24 -0500

    pathfinding stuck in electrical

--- navigate.py
+++ navigate.py
@@ -8,7 +8,12 @@ import numpy as np
 marker = (198, 17, 17)
 marker_arrived = (228, 132, 10)
 tasks = [["Admin Swipe", (641, 337)], 
-        ["Fuel Engine (Storage)", (466, 449)]]
+        ["Fuel Engine (Storage)", (466, 449)],
+        ["Fix Wires (Electrical)", (372, 323)],
+        ["Calibrate Distributor", (410, 323)],
+        ["Divert Power", (329, 323)],
+        ]
+
 def map():
     print("Where would you like to go?:")
     print("[1] Custom Map")
@@ -72,7 +77,9 @@ def custom_map():
     cv2.destroyAllWindows()
 
 def pathfinding():
-    destination = tasks[1][1]
+    
+    #for task in tasks:
+    destination = tasks[0][1]
     img_map_pix = Image.open('result_test_2.jpg')
 
     img_map_pix = Image.open('result_test_2.jpg')
@@ -84,9 +91,10 @@ def pathfinding():
     img[560:600, 628:837] = [0, 0, 0]
     
     pix_map = img_map_pix.load()
-    #Y,X = np.where(np.all(img==marker_arrived, axis=2))
-    #if len(X) == 0:
+    
     Y,X = np.where(np.all(img==marker, axis=2))
+    if len(X) == 0:
+        Y,X = np.where(np.all(img==marker_arrived, axis=2))
     x = 0
     y = 0
 
@@ -103,7 +111,7 @@ def pathfinding():
     for i in path:
         img_map[i[1], i[0]] = (0, 255, 0)
 
-    navigate(path, directions, img_map, destination)
+        #navigate(path, directions, img_map, destination)
 
     
 
@@ -113,6 +121,8 @@ def navigate(path, directions, img_map, destination):
     turns = []
     pyautogui_directions = ["left", "right", "up", "down"]
     time.sleep(2)
+    path = path[2:]
+    directions = directions[2:]
 
     for i in range(len(directions)):
 
@@ -154,16 +164,14 @@ def navigate(path, directions, img_map, destination):
             print("at node")
             if dir != None:
                 pyautogui.keyUp(pyautogui_directions[dir])
-            dir = turns[0][1]
+            dir = turns[3][1]
             turns.pop(0) 
-            
-            
+                    
         pyautogui.keyDown(pyautogui_directions[dir])
 
         cv2.imshow("result", img_map)
         cv2.waitKey(1)
-    cv2.destroyAllWindows()
-        
+    cv2.destroyAllWindows()       
 
 def find_path(current_pos, pix, destination):
     print(current_pos)
@@ -172,13 +180,20 @@ def find_path(current_pos, pix, destination):
     nodes = []
     directions = []
     while True:
+        prior = current_pos
         possible = [(current_pos[0]-1, current_pos[1]), 
                     (current_pos[0]+1, current_pos[1]), 
                     (current_pos[0], current_pos[1]-1), 
                     (current_pos[0], current_pos[1]+1)]
 
-        #for i in possible:
-            #print(pix[i])
+        if len(path) > 3 and path[-1] == path[-3]:
+            print(current_pos)
+            print(pix[437, 450])
+            print(pix[437, 448])
+            print(pix[438, 449])
+            print(pix[436, 449])
+
+            break
         
         if dir != None:
             pix[current_pos] = (0, 0, 0)
@@ -191,19 +206,19 @@ def find_path(current_pos, pix, destination):
         # If just started or can't keep going straight
         elif (dir == None) or ((dir != None) and (pix[possible[dir]] < (10, 10, 10))):
             white_counter = 0
+            
             for moves in possible:
+                #print(pix[moves])
                 if pix[moves] >= (210, 210, 210):
-                    #print("Available move")
-                    #pix[current_pos] = (0, 0, 0)
                     dir = possible.index(moves)
                     current_pos = moves
                     white_counter +=1
-                #print(pix[moves])
+            
+            print(white_counter)
             
             # All sides are black
             if white_counter == 0:
                 if len(nodes) != 0:
-                    print(nodes)
                     node_index = path.index(nodes[-1])
                     path = path[:node_index+1]
                     directions = directions[:node_index+1]
@@ -213,35 +228,27 @@ def find_path(current_pos, pix, destination):
                     break
             
             # 2 or 1 black square
-            elif white_counter == 3 or white_counter == 2:
-                nodes.append(current_pos)
-                for i in possible:
-                    print(pix[i])
-                print("EC Node: " + str(current_pos) + " " + str(white_counter))
-            
+            elif white_counter == 3:
+                nodes.append(prior)
+                print("EC Node: " + str(prior) + " " + str(white_counter))
             path.append(current_pos)
             directions.append(dir)
-            
-        
+                   
         #keep going straight
         else:
             white_counter = 0
-            
             directions.append(dir)
-
             for moves in possible:
                 if pix[moves] >= (210, 210, 210):
                     white_counter += 1
             
             if white_counter >= 2:
-                for i in possible:
-                    print(pix[i])
                 nodes.append(current_pos)
                 print("WC Node: " + str(current_pos))
 
             #pix[current_pos] = (0, 0, 0)
             current_pos = possible[dir]
             path.append(current_pos)
-
         print(current_pos)
+
     return path, directions
